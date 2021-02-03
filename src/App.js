338d557abfe6b86d8598e3de59b1940b06166a3c
Joseph Gao <joseph8p@gmail.com>
2021-02-02 21:36:24 -0500

    added modal, uncomplete

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './style/base.scss';
 // import {themes} from './constant/AppConstant';
 import Landing from './landing/Landing';
 import Project from './project/Project'
+import Modal from './components/Modal'
 
 function App() {
   const [twilightTheme, sTheme] = useState(true)
+  const [displayModal, sModal] = useState(false)
 
   return (
     <div className={twilightTheme?`App__bg_dark App`:`App__bg_light App`}>      
       <Landing theme={twilightTheme} setTheme={sTheme} />
-      <Project />
-      <Project />
-      <Project />
+      <Project displayModal={displayModal} setModal={sModal}/>
+      <Project displayModal={displayModal} setModal={sModal}/>
+      <Project displayModal={displayModal} setModal={sModal}/>
+      <Modal />
     </div>
   );
 }
