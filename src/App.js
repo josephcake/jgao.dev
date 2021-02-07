0f817a2a61cc01f622a3b5c55b481db991bbc011
Joseph Gao <joseph8p@gmail.com>
2021-02-06 21:25:08 -0500

    check project in view

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import dtd_1 from './media/dtd/dtd_1.gif'
 import dtd_2 from './media/dtd/dtd_2.gif'
 
 function App() {
-  const [twilightTheme, sTheme] = useState(true)
-  const [displayModal, tModal] = useState(false)
-  const [firstLoad, checkFirst] = useState(true)
-  const [pageHeight, sPageHeight] = useState(0)
+  const [twilightTheme, sTheme]     = useState(true)
+  const [displayModal, tModal]      = useState(false)
+  const [firstLoad, checkFirst]     = useState(true)
+  const [inView, sInView]   = useState({top:0, project:0})
+  
 
   const debounce = (fn, delay) => {
     let timeoutID;
@@ -29,7 +30,18 @@ function App() {
     checkFirst(false)
   }
   const handleScroll=(e)=>{
-    console.log(e.target.scrollTop)
+    // console.log(e.target.scrollTop)
+    let scrollTop = e.target.scrollTop
+    let prevState = inView;
+    if(scrollTop > inView.top){      
+      prevState.top = scrollTop
+      prevState.project ++
+    }else if(scrollTop < inView.top){      
+      prevState.top = scrollTop;
+      prevState.project--;
+    }
+    sInView(prevState)
+    // console.log(inView)
     // console.log(e.target.scrollHeight)  overall scrollable height
   }
   return (
