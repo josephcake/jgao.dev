958c0c96a6ba93d07934c5e829d781c3c723ff80
Joseph Gao <joseph8p@gmail.com>
2021-02-06 22:19:10 -0500

    zooming & title position

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import dtd_1 from './media/dtd/dtd_1.gif'
 import dtd_2 from './media/dtd/dtd_2.gif'
 
 function App() {
-  const [twilightTheme, sTheme]     = useState(true)
-  const [displayModal, tModal]      = useState(false)
-  const [firstLoad, checkFirst]     = useState(true)
-  const [inView, sInView]   = useState({top:0, project:0})
+  const [twilightTheme, sTheme] = useState(true)
+  const [displayModal, tModal]  = useState(false)
+  const [firstLoad, checkFirst] = useState(true)
+  const [inView, sInView]       = useState({top:0, project:0})
   
 
   const debounce = (fn, delay) => {
@@ -31,28 +31,43 @@ function App() {
   }
   const handleScroll=(e)=>{
     // console.log(e.target.scrollTop)
-    let scrollTop = e.target.scrollTop
-    let prevState = inView;
-    if(scrollTop > inView.top){      
-      prevState.top = scrollTop
-      prevState.project ++
-    }else if(scrollTop < inView.top){      
-      prevState.top = scrollTop;
-      prevState.project--;
+    let scrollTop = e.target.scrollTop    
+    let sectionHeight = e.target.scrollHeight/4
+    let prevState = {...inView};
+    if(scrollTop !== inView.top){      
+      prevState.project = scrollTop/sectionHeight
+      prevState.top = scrollTop      
     }
-    sInView(prevState)
-    // console.log(inView)
-    // console.log(e.target.scrollHeight)  overall scrollable height
+    sInView(prevState)    
   }
+
   return (
-    <div className={twilightTheme ? `bg-dark App` : `bg-light App`} onScroll={debounce(handleScroll,500)}>
-      <Landing theme={twilightTheme} setTheme={sTheme} />      
-      <Project toggleModal={toggleModal} first/>      
-      <Project toggleModal={toggleModal} />
-      <Project toggleModal={toggleModal} />
-      {firstLoad?null:
-        <Modal theme={twilightTheme} displayModal={displayModal} toggleModal={toggleModal}/>
-      }
+    <div
+      className={twilightTheme ? `bg-dark App` : `bg-light App`}
+      onScroll={debounce(handleScroll, 500)}
+    >
+      <Landing theme={twilightTheme} setTheme={sTheme} />
+      <Project
+        toggleModal={toggleModal}
+        prjNum={1}
+        inView={inView.project}
+        first
+      />
+      <Project
+        toggleModal={toggleModal}
+        prjNum={2}
+        inView={inView.project} />
+      <Project
+        toggleModal={toggleModal}
+        prjNum={3}
+        inView={inView.project} />
+      {firstLoad ? null : (
+        <Modal
+          theme={twilightTheme}
+          displayModal={displayModal}
+          toggleModal={toggleModal}
+        />
+      )}
     </div>
   );
 }
--- src/project/Project.js
+++ src/project/Project.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Project = ({toggleModal, first, hero}) =>{
+const Project = ({toggleModal, first, hero, prjNum, inView}) =>{
+  const zoomed = prjNum === inView ? "zoomed" : ""
+  console.log(prjNum, inView)
   return (
     <div id={`${first?'first':null}`}className={`section`}>
       <div className={"project__container"}>
-        <div className={"project__inner"} onClick={toggleModal} >
-          <div className={"project__title"}>{"Title"}</div>
+        <div className={"project__title"}>{"Title"}</div>
+        <div className={`project__inner ${zoomed}`} onClick={toggleModal} >
           <img className={"project__hero"} alt={'dtd'} srcSet={hero}/>
         </div>
       </div>      
