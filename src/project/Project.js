0e78739d4fded9ce28fc4594a2dc62f125952499
Joseph Gao <joseph8p@gmail.com>
2021-02-09 22:59:58 -0500

    pulse effect modal

--- src/project/Project.js
+++ src/project/Project.js
@@ -20,34 +20,34 @@ const Project = ({
         <div className={"project__outer"}>
           <div
             className={`project__inner ${zoomed} ${invert}`}
-            // onClick={toggleModal}
             style={{ backgroundImage: `url(${hero})` }}
           ></div>
         </div>
       </div>
+      //group them together //justify baseline space between
+      <div className={`project_overlay_container`}>
+        <div className={""}>
+          <h6>{subtitle}</h6>
+          <h1>{title}</h1>
+        </div>
 
-      //group them together 
-      //justify baseline space between
-      <div className={`project__inner_info`}>
-        <h6>{subtitle}</h6>
-        <h1>{title}</h1>
-      </div>
-
-      <div className={`project__inner_view`}>
-        <div className={`landing__end`}
-          // onClick={scrollToNext}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="50"
-            height="50"
+        <div className={`project__inner_view`}>
+          <div
+            className={`modal_arrow`}
+            onClick={toggleModal}
           >
-            <path
-              fillRule="evenodd"
-              d="M5.22 8.72a.75.75 0 000 1.06l6.25 6.25a.75.75 0 001.06 0l6.25-6.25a.75.75 0 00-1.06-1.06L12 14.44 6.28 8.72a.75.75 0 00-1.06 0z"
-            ></path>
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="30"
+              height="30"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.22 8.72a.75.75 0 000 1.06l6.25 6.25a.75.75 0 001.06 0l6.25-6.25a.75.75 0 00-1.06-1.06L12 14.44 6.28 8.72a.75.75 0 00-1.06 0z"
+              ></path>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
