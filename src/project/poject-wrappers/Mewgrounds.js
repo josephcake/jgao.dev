f90721fcac111521e7106ee4dd31366b40d30ce6
Joseph Gao <joseph8p@gmail.com>
2021-02-13 21:19:24 -0500

    mewground page

--- src/project/poject-wrappers/Mewgrounds.js
+++ src/project/poject-wrappers/Mewgrounds.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import mewgrounds from '../../media/mewgrounds/mewgrounds_rotated.jpg'
+import charizard from '../../media/mewgrounds/charizard.png'
 
 const Mewgrounds = () =>{
   return (
@@ -12,21 +13,33 @@ const Mewgrounds = () =>{
         />
       </div>
       <div className={"project-item mid-section"}>
-        <div className={"project-info-container project-info-container__left"}>
-          <label>Title</label>
+        <div className={"project-info-container"}>
+          <label className={"project-label"}>Title</label>
           <div className={"project-name"}>Forever I shall Rise</div>
-          <label>Purpose</label>
+          <label className={"project-label"}>Purpose</label>
 
           <div className={"project-purpose"}>
             To showcase some skills, or improve a service.
           </div>
         </div>
-        <div className={"project-info-container project-info-container__right"}>
-          <label>Details</label>
+        <div className={"project-info-container"}>
+          <label className={"project-label"}>Details</label>
 
           <div className={"project-details"}>
             Lorem ipsum bacon fish burger tomatoes and everything nice.
           </div>
+        </div>
+      </div>
+
+      <div className={"project-item mid-section"}>
+        <div className={''}>
+          <img className={"project-img"} srcSet={charizard} alt={"charizard"} />
+        </div>
+        <div className={"project-text-container"}>
+          <p className={'project-p'}>Pokemon Name</p>
+          <p className={'project-p'}>Image</p>
+          <p className={'project-p'}>Pokedex number</p>
+          <p className={'project-p'}>Primary & Secondary types</p>
         </div>
       </div>
     </div>
