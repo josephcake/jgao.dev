3c7be7c929dc9cbd30f5bd9bf74c539e0098f22e
Joseph Gao <joseph8p@gmail.com>
2021-02-13 23:56:37 -0500

    added pokemon_stat_overall;

--- src/project/poject-wrappers/Mewgrounds.js
+++ src/project/poject-wrappers/Mewgrounds.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import mewgrounds from '../../media/mewgrounds/mewgrounds_rotated.jpg'
 import charizard from '../../media/mewgrounds/charizard.png'
+import pokemon_stat_overall from '../../media/mewgrounds/pokemon_stat_overall.png'
+import pokemon_level_up from '../../media/mewgrounds/pokemon_level_up.jpg'
 
 const Mewgrounds = () =>{
   return (
@@ -12,7 +14,8 @@ const Mewgrounds = () =>{
           alt={"mewgrounds"}
         />
       </div>
-      <div className={"project-item mid-section"}>
+
+      <div className={"project-item mid-section_dual"}>
         <div className={"project-info-container"}>
           <label className={"project-label"}>Title</label>
           <div className={"project-name"}>Forever I shall Rise</div>
@@ -31,15 +34,40 @@ const Mewgrounds = () =>{
         </div>
       </div>
 
-      <div className={"project-item mid-section"}>
-        <div className={''}>
+      <div className={"project-item mid-section_dual"}>
+        <div className={""}>
           <img className={"project-img"} srcSet={charizard} alt={"charizard"} />
         </div>
         <div className={"project-text-container"}>
-          <p className={'project-p'}>Pokemon Name</p>
-          <p className={'project-p'}>Image</p>
-          <p className={'project-p'}>Pokedex number</p>
-          <p className={'project-p'}>Primary & Secondary types</p>
+          <p className={"project-p"}>Pokemon Name</p>
+          <p className={"project-p"}>Image</p>
+          <p className={"project-p"}>Pokedex number</p>
+          <p className={"project-p"}>Primary & Secondary types</p>
+        </div>
+      </div>
+
+      <div className={"project-item mid-section expand"}>
+        <div className={""}>
+          <img
+            className={""}
+            srcSet={pokemon_level_up}
+            alt={"pokemon_level_up"}
+          />
+        </div>
+        <div className={"project-text-container"}>
+          <p className={"project-p"}>Some details about the above image</p>
+        </div>
+      </div>
+      <div className={"project-item mid-section expand"}>
+        <div className={""}>
+          <img
+            className={""}
+            srcSet={pokemon_stat_overall}
+            alt={"pokemon_stat_overall"}
+          />
+        </div>
+        <div className={"project-text-container"}>
+          <p className={"project-p"}>Some details about the above image</p>
         </div>
       </div>
     </div>
