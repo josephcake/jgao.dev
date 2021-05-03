56ec9c9f8c0da91ef9ef429c3925b403dc2684b1
Joseph Gao <joseph8p@gmail.com>
2021-05-03 19:59:38 -0400

    about me, experience UI

---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ExperienceCard = ({title, companyLink, company, timeEmployed, summary, tasks}) =>{
+  const allTasks = tasks.map(t=>(
+    <li>{t}</li>
+  ))
+  return (
+    <div className={"exp-container"}>
+      <h1 className={"exp__header"}>{title}</h1>
+      <h4 className={"exp__subheader"}>
+        <a href={companyLink} target={"_blank"}>
+          {company}
+        </a>
+        <span className={"exp__subheader__year"}>{timeEmployed}</span>
+      </h4>
+      <h3 className={"exp__summary"}>{summary}</h3>
+      <ul className={"exp__ul"}>
+        {allTasks}
+      </ul>
+    </div>
+  );
+}
+export default ExperienceCard
