a67ee62cdbf477965efb0602008b9eb455873700
joseph gao <joseph8p@gmail.com>
2023-03-04 21:59:18 -0800

    update

--- src/project/about-me-wrappers/ExpTab.js
+++ src/project/about-me-wrappers/ExpTab.js
@@ -9,12 +9,16 @@ const ExpTab = () => {
 				companyLink={'https://rapidsos.com/'}
 				company={'RapidSOS'}
 				timeEmployed={'(Apr 2020 - Present)'}
-				summary={'An emergency response data platform'}
+				summary={'An intelligent emergency response data platform'}
 				tasks={[
-					'Monitor, maintain and improve the architectures of current cloud services',
-					'Assist the engineering team to design and build new services with deployment, availability, and scalability in mind',
-					'Integrate and centralize multiple monitoring tools to rapidly identify the root of cause and reduce redundancy',
-					'Developed reusable React and Styled-Component codes while maintaining brand consistency throughout the design',
+					'Designed and developed reusable components for Google and ESRI maps that effectively pinpointed locations using diverse variables such as caller location, alerts, Ani/Ali, and what3words.',
+					'Integrated with third-party APIs to generate dynamic layers on top of Google and ESRI maps, elevating their functionality and enhancing the overall user experience.',
+					'Collaborated with cross-functional teams to create and implement new services and demos with a focus on deployment, availability, and scalability.',
+					'Increased engagement with ECCs from 26% to 60% by building dynamic components with WebAPIs, laying the groundwork for future partnerships.',
+					'Integrated and reverse geocoded third-party systems to ensure the accurate geographic location data of mobile, Ani/Ali, and vehicle information.',
+					'Utilized Storybook to isolate, mock, document, and test components to optimize productivity and efficiency.',
+					'Maintained and improved the architecture of cloud services to guarantee high system availability.',
+					'Streamlined and consolidated monitoring tools, reducing redundancy by over 40% and speeding up the root cause identification process, resulting in increased collaboration within a shorter turnaround time.',
 				]}
 			/>
 			<ExperienceCard
@@ -24,9 +28,10 @@ const ExpTab = () => {
 				timeEmployed={'(Aug 2019 - Apr 2020)'}
 				summary={'A Steward of the Internet of Education'}
 				tasks={[
-					'Conversion of legacy JavaScript layout over to modern React structure',
-					'Developing interactive UI and reusable components with React while maintaining brand consistency throughout the design',
-					'Work closely with the design team to create mockups, prototypes, and final-build',
+					'Collaborated with the design team to create mockups, prototypes, and final builds using Webflow for both internal sites and clients.',
+					'Worked closely with the CTO to standardize the front-end design system and infrastructure.',
+					'Developed mobile-first interactive UI and reusable components with React while ensuring consistency with the brand guidelines and design standards.',
+					'Successfully converted legacy JavaScript and HTML layout into a modern React structure, optimizing performance and maintainability.',
 				]}
 			/>
 		</div>
